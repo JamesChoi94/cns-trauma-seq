8b9eaccdbf71f9ec5ab7e23d20134682b89bf4f1
JamesChoi <seungchoi94@gmail.com>
2022-08-29 17:39:49 -0400

    meh

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # cns-trauma-seq
 
 Single cell sequencing resource of data from central nervous system traumatic injury.
-
---
+++ pegasus-bsub_milich2021-download.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#BSUB -J cns-trauma-seq_download-data
+#BSUB -P lemmon
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -W 4:00
+#BSUB -q general
+#BSUB -n 4
+#BSUB -B
+#BSUB -N
+#BSUB -u jsc228@miami.edu
+
+####################################################
+# Set directory variables and conda env
+####################################################
+
+cd /scratch/projects/lemmon/jsc228/cns-trauma-seq
+mkdir data
+cd data
+mkdir bams
+cd /scratch/projects/lemmon/jsc228/cns-trauma-seq
+
+####################################################
+# Job
+####################################################
+
+wget --input-file=milich2021-ftp-urls.txt --directory-prefix=data/bams
+
+####################################################
+# Script End
+####################################################
