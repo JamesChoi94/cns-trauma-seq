265852f958eebce8723f0b9c28efdd058c36e4f7
JamesChoi <seungchoi94@gmail.com>
2022-08-29 18:20:07 -0400

    curl download

---
+++ milich2021-ftp-urls.sh
@@ -0,0 +1,10 @@
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR131/SRR13192645/uninj_sample1_possorted_genome_bam.bam.1 -o uninj_sample1_possorted_genome_bam.bam.1
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR131/SRR13192646/uninj_sample2_possorted_genome_bam.bam.1 -o uninj_sample2_possorted_genome_bam.bam.1
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR131/SRR13192647/uninj_sample3_possorted_genome_bam.bam.1 -o uninj_sample3_possorted_genome_bam.bam.1
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR131/SRR13192648/1dpi_sample1_possorted_genome_bam.bam.1 -o 1dpi_sample1_possorted_genome_bam.bam.1
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR131/SRR13192649/1dpi_sample2_possorted_genome_bam.bam.1 -o 1dpi_sample2_possorted_genome_bam.bam.1
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR131/SRR13192650/1dpi_sample3_possorted_genome_bam.bam.1 -o 1dpi_sample3_possorted_genome_bam.bam.1
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR131/SRR13192651/3dpi_sample1_possorted_genome_bam.bam.1 -o 3dpi_sample1_possorted_genome_bam.bam.1
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR131/SRR13192652/3dpi_sample2_possorted_genome_bam.bam.1 -o 3dpi_sample2_possorted_genome_bam.bam.1
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR131/SRR13192653/7dpi_sample1_possorted_genome_bam.bam.1 -o 7dpi_sample1_possorted_genome_bam.bam.1
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR131/SRR13192654/7dpi_sample2_possorted_genome_bam.bam.1 -o 7dpi_sample2_possorted_genome_bam.bam.1
--- pegasus-bsub_milich2021-download.sh
+++ pegasus-bsub_milich2021-download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#BSUB -J cns-trauma-seq_download-data
+#BSUB -J cns-trauma-seq_download-data-curl
 #BSUB -P lemmon
 #BSUB -o %J.out
 #BSUB -e %J.err
 #BSUB -W 4:00
 #BSUB -q general
-#BSUB -n 4
+#BSUB -n 8
 #BSUB -B
 #BSUB -N
 #BSUB -u jsc228@miami.edu
@@ -24,7 +24,7 @@ cd /scratch/projects/lemmon/jsc228/cns-trauma-seq
 # Job
 ####################################################
 
-wget --input-file=milich2021-ftp-urls.txt --directory-prefix=data/bams
+cat milich2021-ftp-urls.sh | parallel -j 8 "{}"
 
 ####################################################
 # Script End
